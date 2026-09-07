651616fe60bfe50e754524768289e7feafe0246c
112413005 <112413005@kanchiuniv.ac.in>
2026-09-07 16:14:42 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-```javascript
+
 // ================= SECTION NAVIGATION =================
 
 function showSection(sectionId, clickedItem) {
@@ -126,4 +126,4 @@ function logoutAdmin() {
     }
 
 }
-```
+
